864d80204c48eb5f2f1da93af6963530dc2e6a12
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-03-17 08:25:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-n3rs8h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-n3rs8h)
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const [chkBox, setChkBox] = useState(false);
+  const [values, setValues] = useState({
+    message: '',
+    chktest: chkBox,
+  });
+  const [isSubmit, setIsSubmit] = useState(false);
+  const handleChanges = (e) => {
+    setChkBox(e.target.checked);
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+  const handleChanges1 = (e) => {
+    setChkBox(e.target.checked);
+    const name = e.target.name;
+    const value = e.target.checked;
+    setValues({ ...values, [name]: value });
+  };
+  const submitdata = (e) => {
+    e.preventDefault();
+    setIsSubmit(true);
+    if (!isSubmit) {
+      console.log('israr hey');
+      console.log('values of check click' + chkBox);
+      console.log('values of check click innner' + values.chktest);
+      const allData = { ...values };
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(allData),
+      };
+      console.log(requestOptions.body);
+      fetch('https://reqres.in/api/posts', requestOptions)
+        .then((response) => {
+          console.log('post sucess' + response);
+          response.json();
+        })
+        //.then((data) => setValues(data.message))
+        .catch((error) => {
+          console.log('failed result' + error);
+        });
+    }
+  };
+  return (
+    <form onSubmit={submitdata}>
+      <h1>Hello StackBlitz!</h1>
+      <p>
+        <input
+          type="text"
+          onChange={handleChanges}
+          value={values.message}
+          name="message"
+        ></input>
+      </p>
+      <p>
+        <input
+          type="checkbox"
+          onChange={handleChanges1}
+          value={values.chktest}
+          name="chktest"
+        ></input>
+      </p>
+      <p>
+        <input type="submit" />
+      </p>
+    </form>
+  );
+}
