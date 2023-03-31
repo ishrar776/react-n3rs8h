f6fc0a9244b2335ce440f07e1f1ce2a33cb6b53c
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-03-31 17:14:35 +0530

    2 files modified, 1 added

--- src/registerUser.js
+++ src/registerUser.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './style.css';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import EnhancedTable from './tableData';
 export default function RegisterUser() {
   let navigate = useNavigate();
   return (
     <>
       <div>Details List of all RegisterUser</div>
       <p>
-        <Button onClick={() => navigate('/register')}>
+        <Button variant="contained" onClick={() => navigate('/register')}>
           Back To Register Deatils Page{' '}
         </Button>
       </p>
       <p>Register user Details 2023</p>
+      <EnhancedTable />
     </>
   );
 }
