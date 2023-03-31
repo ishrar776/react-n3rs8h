5d6b1ed00e892ab759660534c14e5dc0f9a09acd
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-03-31 11:11:50 +0530

    1 file modified

--- src/customerthanks.js
+++ src/customerthanks.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './style.css';
 import { useNavigate, Link } from 'react-router-dom';
+import styled from 'styled-components';
 export default function CustomerThank() {
+  const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+  const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
   let navigate = useNavigate();
   return (
     <>
+      <Wrapper>
+        <Title>Hello World!</Title>
+      </Wrapper>
+
       <div>thanks customer we register your profile please check your mail</div>
       <p>
         <button onClick={() => navigate('/App')}>Back To DashBoard </button>
