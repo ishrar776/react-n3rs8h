348f7d63a8d17d834631698e8ea2e57bef545249
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-03-31 11:18:10 +0530

    1 file modified

--- src/customerthanks.js
+++ src/customerthanks.js
@@ -12,20 +12,33 @@ export default function CustomerThank() {
   padding: 4em;
   background: papayawhip;
 `;
+  const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? 'palevioletred' : 'white')};
+  color: ${(props) => (props.primary ? 'white' : 'palevioletred')};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
   let navigate = useNavigate();
   return (
     <>
       <Wrapper>
         <Title>Hello World!</Title>
-      </Wrapper>
 
-      <div>thanks customer we register your profile please check your mail</div>
-      <p>
-        <button onClick={() => navigate('/App')}>Back To DashBoard </button>
-      </p>
-      <p>
-        <Link to={'/RegisterUser'}>Details Registered User</Link>
-      </p>
+        <div>
+          thanks customer we register your profile please check your mail
+        </div>
+        <p>
+          <Button onClick={() => navigate('/App')}>Back To DashBoard </Button>
+        </p>
+        <p>
+          <Link to={'/RegisterUser'}>Details Registered User</Link>
+        </p>
+      </Wrapper>
     </>
   );
 }
